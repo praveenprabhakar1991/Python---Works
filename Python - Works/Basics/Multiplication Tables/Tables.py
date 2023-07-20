9cf6e9960e4e9bf9c349f87a056cb99a36abcdea
praveenprabhakar1991 <pprabhakar108@gmail.com>
2023-07-21 02:11:22 +0530

    Final

---
+++ Python - Works/Basics/Multiplication Tables/Tables.py
@@ -0,0 +1,5 @@
+num = int(input("Enter any Number : "))
+print (f"The Multiplication Tables of {num} is Shown Below.")
+
+for i in range (1,11):
+    print (f"{num} * {i} = {num * i}")
---
+++ Python - Works/Basics/Multiplication Tables/Tables_Try_Except.py
@@ -0,0 +1,10 @@
+num = input("Enter any Number : ")
+print (f"The Multiplication of {num} is :")
+
+try:
+    for i in range (1,11):
+        print (f"{int(num)} * {i} = {int(num) * i}")
+        
+except:
+    print ("Invalid Input!!!")
+    print ("Enter only Numbers")
