8223cadb6553ab15f4eed4b86eae85b29e7f5d6d
Prabhakar B <pprabhakar108@gmail.com>
2023-09-16 01:49:46 +0530

    Final Commit

--- Python - Works/Basics/Martingale.py
+++ Python - Works/Basics/Martingale.py
@@ -1,11 +1,13 @@
 # WAP for Martingale Sequence [1,2,4,8,16,32,64,128,256,512,1024,...]
 
+n = int(input('Enter the Number for Martingale Sequence : '))
+
 a = 1
-b = 2
+b = 5
 
 sequence = []
 
-for _ in range(10):
+for _ in range(n):
     sequence.append(a)
     a = a * b
     
