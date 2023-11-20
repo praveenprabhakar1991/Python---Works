de3762cf547594f88fbf91f08b75309b1c762396
Prabhakar B <pprabhakar108@gmail.com>
2023-11-21 00:25:26 +0530

    Final Commit

--- Python - Works/Basics/Os_module_rename.py
+++ Python - Works/Basics/Os_module_rename.py
@@ -4,12 +4,16 @@ import shutil
 cwd = os.getcwd()                                   # To know the Current Working Directory
 print("The Current Working Directory is ", cwd ) 
 
-if not os.path.exists('Data'):                      # Doesn't show error after executing next time
-    os.mkdir('Data')
-    
-# for i in range(100):                      
-#     os.mkdir(f'Data/{i+1}')                         # It creates Sub-Folders inside Current Folder
+# if not os.path.exists('Data'):                      # Doesn't show error after executing next time
+#     os.mkdir('Data')
    
+files = os.listdir("Data")
+i = 1
+    
+for file in files:
+    if file.endswith(".jpg"):
+        os.rename(f'Data/{file}', f'Data/{i}.pdf')   # It renames the Folders/Files
+        i = i + 1
 # os.remove()                                       # It removes a single file
 # os.rmdir()                                        # It removes an empty directory.    
 # shutil.rmtree('Data')                             # It deletes a directory and all its contents.
